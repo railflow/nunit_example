320d1206a4e69d1637a7f6b2da17052c24ad7835
vaheh <vaheh@DESKTOP-1F65PV4>
2021-12-06 17:53:19 +0200

    TestRailIds to int array

--- src/Railflow.NUnit.TestRail.Reporter.Example/Example/RailflowAttributeExample.cs
+++ src/Railflow.NUnit.TestRail.Reporter.Example/Example/RailflowAttributeExample.cs
@@ -5,11 +5,6 @@ namespace Example
 {
     /// <summary>
     /// NOTE: 'JiraIds' isn't class-level marker. So will be ignored
-    /// <properties>
-    ///     <property name = "railflow-title" value="class-title" />
-    ///     <property name = "railflow-case-fields" value="class-case-field-1" />
-    ///     <property name = "railflow-case-fields" value="class-case-field-2" />
-    /// </properties>
     /// </summary>
     [Railflow(
         Title = "class-title",
@@ -21,38 +16,17 @@ namespace Example
         public void Setup()
         {
         }
-
-        /// <summary>
-        /// Markers:
-        /// <properties>
-        ///     <property name="railflow-title" value="func-title" />
-        ///     <property name="railflow-case-fields" value="class-case-field-1" />
-        ///     <property name="railflow-case-fields" value="class-case-field-2" />
-        ///     <property name="railflow-case-priority" value="func-case-priority" />
-        ///     <property name="railflow-test-rail-ids" value="func-test-rail-id-1" />
-        ///     <property name="railflow-test-rail-ids" value="func-test-rail-id-2" />
-        ///     <property name="railflow-jira-ids" value="func-jira-id-1" />
-        ///     <property name="railflow-jira-ids" value="func-jira-id-2" />
-        /// </properties>
-        /// </summary>
+        
         [Railflow(
             Title = "func-title",
             CasePriority = "func-case-priority",
-            TestRailIds = new[] { "func-test-rail-id-1", "func-test-rail-id-2" },
+            TestRailIds = new[] { 1, 2 },
             JiraIds = new[] { "func-jira-id-1", "func-jira-id-2" })]
         [Test]
         public void MarkerExample1()
         {
         }
 
-        /// <summary>
-        /// Markers:
-        /// <properties>
-        ///     <property name = "railflow-title" value="class-title" />
-        ///     <property name = "railflow-case-fields" value="class-case-field-1" />
-        ///     <property name = "railflow-case-fields" value="class-case-field-2" />
-        /// </properties>
-        /// </summary>
         [Test]
         public void MarkerExample2()
         {
